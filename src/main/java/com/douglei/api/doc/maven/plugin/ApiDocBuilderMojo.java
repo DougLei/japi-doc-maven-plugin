4d0aab2e0a1dcf6119385ab335e8bde9e83eb979
DougLei <953034206@qq.com>
2019-10-22 16:45:59 +0800

    t

--- src/main/java/com/douglei/api/doc/maven/plugin/ApiDocBuilderMojo.java
+++ src/main/java/com/douglei/api/doc/maven/plugin/ApiDocBuilderMojo.java
@@ -1,12 +1,20 @@
 package com.douglei.api.doc.maven.plugin;
 
+import java.io.IOException;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.douglei.api.doc.js.variable.entity.apis.common.CommonParamEntity;
+import com.douglei.api.doc.ApiDocBuilder;
+import com.douglei.api.doc.ApiFolderBuilder;
+import com.douglei.api.doc.ApiZipBuilder;
+import com.douglei.tools.utils.ExceptionUtil;
 
 /**
  * 
@@ -14,27 +22,144 @@ import com.douglei.api.doc.js.variable.entity.apis.common.CommonParamEntity;
  */
 @Mojo(name = "apiDocBuilder", defaultPhase = LifecyclePhase.COMPILE)
 public class ApiDocBuilderMojo extends AbstractMojo {
+	private static final Logger logger = LoggerFactory.getLogger(ApiDocBuilderMojo.class);
 	
-	protected String name;
-	protected String path;
+	/**
+	 * 生成api文档的类型, 值包括 [zip, folder], 不区分大小写, 默认值为zip
+	 * zip标识生成压缩包, folder标识生成文件夹
+	 */
+	@Parameter
+	private String apiDocType = "zip";
+	
+	/**
+	 * 生成api文档的名称和标题, 默认名为 api文档
+	 */
+	@Parameter
+	private String name;
+	/**
+	 * 生成api文档的绝对路径, 默认在当前项目的target目录下
+	 */
+	@Parameter
+	private String path;
 
+	/**
+	 * api作者, 默认为当前计算机的名称
+	 */
+	@Parameter
 	private String[] authors;
+	/**
+	 * 版本, 默认为1.0
+	 */
+	@Parameter
 	private String version;
+	
+	/**
+	 * api开发环境的url
+	 */
+	@Parameter
 	private String[] devEnvironmentUrls;
+	/**
+	 * api测试环境的url
+	 */
+	@Parameter
 	private String[] testEnvironmentUrls;
+	/**
+	 * api生产环境的url
+	 */
+	@Parameter
 	private String[] prodEnvironmentUrls;
 	
-	private CommonParamEntity commonHeader;
-	private CommonParamEntity commonUrl;
-	private CommonParamEntity commonRequest;
-	private CommonParamEntity commonResponse;
+	/**
+	 * 通用的请求头配置类
+	 */
+	@Parameter
+	private CommonParam header;
+	/**
+	 * 通用的请求URL配置类
+	 */
+	@Parameter
+	private CommonParam url;
+	/**
+	 * 通用的请求体配置类
+	 */
+	@Parameter
+	private CommonParam request;
+	/**
+	 * 通用的响应体配置类
+	 */
+	@Parameter
+	private CommonParam response;
 	
+	/**
+	 * 要扫描的包路径, 默认为当前项目的根包名
+	 */
+	@Parameter
 	private String[] scanPackages;
 	
 	
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		// TODO Auto-generated method stub
-
+		ApiDocBuilder builder = getBuilder();
+		if(name != null) {
+			builder.setName(name);
+		}
+		if(path != null) {
+			builder.setPath(path);
+		}
+		
+		if(arrayNotEmpty(authors)) {
+			builder.setAuthors(authors);
+		}
+		if(version != null) {
+			builder.setVersion(version);
+		}
+		
+		if(arrayNotEmpty(devEnvironmentUrls)) {
+			builder.setDevEnvironmentUrls(devEnvironmentUrls);
+		}
+		if(arrayNotEmpty(testEnvironmentUrls)) {
+			builder.setTestEnvironmentUrls(testEnvironmentUrls);
+		}
+		if(arrayNotEmpty(prodEnvironmentUrls)) {
+			builder.setProdEnvironmentUrls(prodEnvironmentUrls);
+		}
+		
+		if(header != null && header.unEmpty()) {
+			builder.setCommonHeader(header.getStructureType(), header.getClz());
+		}
+		if(url != null && url.unEmpty()) {
+			builder.setCommonUrl(url.getStructureType(), url.getClz());
+		}
+		if(request != null && request.unEmpty()) {
+			builder.setCommonRequest(request.getStructureType(), request.getClz());
+		}
+		if(response != null && response.unEmpty()) {
+			builder.setCommonResponse(response.getStructureType(), response.getClz());
+		}
+		
+		if(arrayNotEmpty(scanPackages)) {
+			builder.setScanPackages(scanPackages);
+		}
+		
+		try {
+			builder.build();
+			logger.info("api文档创建完成");
+		} catch (IOException e) {
+			logger.error("api文档创建时出现异常: {}", ExceptionUtil.getExceptionDetailMessage(e));
+		}
+	}
+	
+	// 获取builder实例
+	private ApiDocBuilder getBuilder() {
+		if("folder".equalsIgnoreCase(apiDocType)) {
+			return new ApiFolderBuilder();
+		}else {
+			return new ApiZipBuilder();
+		}
+	}
+	
+	// 判断array是否不为空
+	private boolean arrayNotEmpty(String[] array) {
+		return array != null && array.length > 0;
 	}
 }
---
+++ src/main/java/com/douglei/api/doc/maven/plugin/CommonParam.java
@@ -0,0 +1,41 @@
+package com.douglei.api.doc.maven.plugin;
+
+import com.douglei.api.doc.enums.ParamStructureType;
+import com.douglei.tools.utils.reflect.ClassLoadUtil;
+import com.douglei.tools.utils.reflect.ValidationUtil;
+
+/**
+ * 通用参数
+ * @author DougLei
+ */
+public class CommonParam {
+	
+	/**
+	 * 通用参数的结构, 值包括 [object, array, object_or_array], 不区分大小写, 默认值为object
+	 * object标识为对象结构, array标识为数组/集合结构, object_or_array标识为对象或数组/集合结构
+	 */
+	private String structureType = "object";
+	
+	/**
+	 * 对应配置类的全路径
+	 */
+	private String clz;
+	
+	public boolean unEmpty() {
+		return ValidationUtil.classExists(clz);
+	}
+	
+	public ParamStructureType getStructureType() {
+		if("array".equalsIgnoreCase(structureType)) {
+			return ParamStructureType.ARRAY;
+		}else if("object_or_array".equalsIgnoreCase(structureType)) {
+			return ParamStructureType.OBJECT_OR_ARRAY;
+		}else {
+			return ParamStructureType.OBJECT;
+		}
+	}
+	
+	public Class<?> getClz(){
+		return ClassLoadUtil.loadClass(clz);
+	}
+}
